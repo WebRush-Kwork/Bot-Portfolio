4bb28de746082f87ac8be78b126bcb01b6c74b17
gleb <webrushgithub@gmail.com>
2024-04-21 16:22:56 +0500

    глеб - базовая структура

---
+++ config.py
@@ -0,0 +1,1 @@
+bot_token = '6398233173:AAEG3A8es881BtATdiLbHtRRBUijN2Oy3gc'
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print('test')
+import sqlite3
+from telebot import TeleBot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
+from logic import *
+from config import *
+
+bot = TeleBot(bot_token)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(
+        message.chat.id, f'Привет, @{message.from_user.username}!\nЗдесь ты можешь надёжно сохранять свои проекты! Надежное хранилище для твоего творчества!')
+
+
+bot.infinity_polling()
